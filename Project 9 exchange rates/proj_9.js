f3b9382b80bf66d0b90768b1931c7ed61ad419da
Kozubowicz <55114447+Kozubowicz@users.noreply.github.com>
2023-05-31 22:08:43 +0200

    add exchange rate projects

---
+++ Project 9 exchange rates/proj_9.js
@@ -0,0 +1,43 @@
+const url = `http://api.nbp.pl/api/exchangerates/tables/a/last?format=json`;
+
+window.onload = function () {
+  getData(url);
+};
+
+function getData(url) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => currency.updateRates(data));
+}
+
+class Currency {
+  currencyData = [];
+  num = null;
+  date = null;
+
+  updateRates(data) {
+    this.currencyData = data[0].rates;
+    this.num = data[0].no;
+    this.date = data[0].effectiveDate;
+    console.log(data);
+    this.printTable();
+  }
+
+  printTable() {
+    document.getElementById(
+      "document"
+    ).innerHTML = `Tabela kursów walut NBP nr: ${this.num} z dnia: ${this.date} <br>`;
+
+    for (let [lp, el] of this.currencyData.entries()) {
+      document.getElementById("currentTable").innerHTML += `   
+    <tr>
+          <td>${lp}.</td>
+          <td>${el.currency}</td>
+          <td>${el.code}</td>
+          <td>${el.mid}</td>
+        </tr>
+    `;
+    }
+  }
+}
+const currency = new Currency();
