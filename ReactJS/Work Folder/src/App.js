3ea4dc34191ad5111c8d51ce613f2c650d4eaffc
Kozubowicz <55114447+Kozubowicz@users.noreply.github.com>
2023-07-27 11:47:31 +0200

    upgrade primal repository 27_07_23

--- ReactJS/Work Folder/src/App.js
+++ ReactJS/Work Folder/src/App.js
@@ -1,17 +1,75 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from "./components/NavBar";
+import Period from "./components/Period";
+import Expenses from "./components/Expenses";
+import History from "./components/History";
 import Footer from "./components/Footer";
 
-export const MyContext = React.createContext();
-
 export default function App() {
-  const [cart, setCart] = useState([]);
+  const [data, setData] = useState([]);
+  const [selectedPeriod, setSelectedPeriod] = useState("");
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem("expenseData"));
+    if (storedData) {
+      setData(storedData);
+      setSelectedPeriod(storedData[0]?.title || "");
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("expenseData", JSON.stringify(data));
+  }, [data]);
+
+  const handleDataChange = (updatedData) => {
+    setData(updatedData);
+  };
+
+  const handlePeriodChange = (period) => {
+    setSelectedPeriod(period);
+  };
+
+  const handleAddPeriod = (period) => {
+    setData((prevData) => [period, ...prevData]);
+    setSelectedPeriod(period.title);
+  };
+
+  const handleDeletePeriod = (periodTitle) => {
+    setData((prevData) => {
+      const newData = prevData.filter((period) => period.title !== periodTitle);
+      return newData;
+    });
+    setSelectedPeriod("");
+  };
+
+  const handleDeleteExpense = (expense) => {
+    const updatedData = [...data];
+    const periodIndex = updatedData.findIndex((period) => period.title === selectedPeriod);
+
+    if (periodIndex !== -1) {
+      const period = updatedData[periodIndex];
+      const expenseIndex = period.expenses.findIndex((item) => item.id === expense.id);
+
+      if (expenseIndex !== -1) {
+        period.expenses = period.expenses.filter((item, index) => index !== expenseIndex);
+        handleDataChange(updatedData);
+      }
+    }
+  };
+
   return (
-    <MyContext.Provider value={{ cart }}>
-      <div className="App">
-        <NavBar />
-        <Footer />
-      </div>
-    </MyContext.Provider>
+    <div className="App">
+      <NavBar />
+      <Period
+        data={data}
+        selectedPeriod={selectedPeriod}
+        onPeriodChange={handlePeriodChange}
+        onAddPeriod={handleAddPeriod}
+        onDeletePeriod={handleDeletePeriod}
+      />
+      <Expenses data={data} selectedPeriod={selectedPeriod} onDataChange={handleDataChange} />
+      <History data={data} selectedPeriod={selectedPeriod} onDeleteExpense={handleDeleteExpense} />
+      <Footer />
+    </div>
   );
 }
--- ReactJS/Work Folder/src/components/NavBar.js
+++ ReactJS/Work Folder/src/components/NavBar.js
@@ -1,37 +1,16 @@
-import React, { useContext } from "react";
+import React from "react";
 import Logo from "./../images/Logo.png";
-import Cart from "./../images/Cart.png";
-import { MyContext } from "../App";
 
 export default function NavBar() {
-  const { cart } = useContext(MyContext);
   return (
     <nav>
-      <div className="navContainer">
-        <div className="NavLogo">
-          <a href="/">
-            <img src={Logo} className="icon" alt="Logo" />
-          </a>
-          <h3 className="NavTile">Online shop</h3>
-        </div>
-        <h3>Project XIV</h3>
+      <div className="NavLogo">
+        <a href="/">
+          <img src={Logo} className="icon" alt="Logo" />
+        </a>
+        <h3 className="NavTile">Expenses tracker</h3>
       </div>
-      <div className="navContainer">
-        <div>
-          <button>Home</button>
-          <button>Store</button>
-          <button>About</button>
-        </div>
-        <div>
-          {cart && cart.length > 0 ? (
-            <button className="cartButton">
-              <img src={Cart} width={20}></img>
-            </button>
-          ) : (
-            <></>
-          )}
-        </div>
-      </div>
+      <h3>Project VIII</h3>
     </nav>
   );
 }
